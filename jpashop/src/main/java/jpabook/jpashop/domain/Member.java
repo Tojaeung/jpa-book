cc6021749a89f6c7f0aac88b73f73802d18ca12f
Tojaeung <kimwoon11@naver.com>
2022-08-19 20:58:41 +0900

    회원도메인 테스트

--- jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -22,7 +22,7 @@ public class Member {
     @Embedded
     private Address address;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 
 
