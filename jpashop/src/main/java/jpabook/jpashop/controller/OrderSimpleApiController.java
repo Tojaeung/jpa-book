768d1f52f609148fb397e8c75e378fbfe7f6e770
Tojaeung <kimwoon11@naver.com>
2022-08-21 15:08:31 +0900

    지연로딩과 조회성능 최적화

---
+++ jpashop/src/main/java/jpabook/jpashop/controller/OrderSimpleApiController.java
@@ -0,0 +1,93 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * xToOne (ManyToOne, OneToOne)
+ * Order -> Member
+ * Order -> Delivery
+ */
+
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+
+        for (Order order : orders) {
+            order.getMember().getName();  // lazy 초기화
+            order.getDelivery().getAddress(); // lazy 초기화
+        }
+
+        /*
+         * 엔티티로 내보내는것이 아니라 DTO로 변환해서 내보낸다
+         * api스펙문제 , 엔티티 변경 문제
+         * */
+        return orders;
+    }
+
+    @GetMapping("/api/v2/orders")
+    public List<SimpleOrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+
+        /*
+         * 엔티티를 DTo로 변화해서 내보낸다.
+         * 지연로딩 영향으로 최악의 경우 N + 1 문제가 발생한다.
+         * 쿼리가 많이 나가서 성능문제가 발생한다. , 네트워크 문제도 발생
+         * */
+        return result;
+    }
+
+    @GetMapping("/api/v3/orders")
+    public List<SimpleOrderDto> orderV3() {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+
+        /*
+         * 페치조인을 이용해서 지연로딩의 N + 1 문제를 해결한다.
+         * 쿼리는 1번 밖에 안나간다.
+         * */
+        return result;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            this.orderId = order.getId();
+            this.name = order.getMember().getName();  // lazy 초기화
+            this.orderDate = order.getOrderDate();
+            this.orderStatus = order.getStatus();
+            this.address = order.getDelivery().getAddress();  // lazy 초기화
+        }
+    }
+}
